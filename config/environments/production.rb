ea331b7e27989321c9d570e65f9cd21d26f91369
NeilTheSeal <neil.hendren@gmail.com>
2024-10-25 19:02:06 -0600

    Possibly fix ingress controller issue?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Prepare the ingress controller used to receive mail
-  # config.action_mailbox.ingress = :relay
+  config.action_mailbox.ingress = :relay
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -90,6 +90,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.action_mailbox.ingress = :relay
 end
