0769d718674d1dd61f2e690a38588f64efd5b80d
NeilTheSeal <neil.hendren@gmail.com>
2024-11-11 10:40:23 -0700

    Create test mailer in production

--- app/controllers/api/v0/advice_controller.rb
+++ app/controllers/api/v0/advice_controller.rb
@@ -3,9 +3,25 @@ require "json"
 class Api::V0::AdviceController < ApplicationController
   def index
     email_info = JSON.parse(request.body.read, symbolize_names: true)
-    message = email_info[:text]
-    subject = email_info[:subject]
+
     from = email_info[:from][/[^<]*@[^>]*/, 0]
-    p from
+    subject = email_info[:subject]
+    content = email_info[:text]
+
+    incoming_email = IncomingEmail.create!(
+      from:,
+      subject:,
+      content:
+    )
+
+    outgoing_email = incoming_email.create_outgoing_email!(
+      email: from,
+      subject: "example message",
+      content: "example content"
+    )
+
+    AdviceMailer.send_advice(outgoing_email).deliver_now
+
+    render json: { message: "Email sent" }, status: 200
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,11 @@ end
 
 require "simplecov"
 SimpleCov.start
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { decode_compressed_response: true }
+end
